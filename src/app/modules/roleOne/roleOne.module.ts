464f694ae2d89a4eeb8c96917e9362d5bf73fb6e
林长茂 <>
2022-02-24 16:53:10 +0800

    lcm

--- src/app/modules/roleOne/roleOne.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RoleOneRoutingModule } from './roleOne-routing.module';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, RoleOneRoutingModule]
-})
-export class RoleOneModule {}
